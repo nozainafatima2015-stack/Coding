c330a4fe036c28e493ace7178c25939a2af825c4
bunny7887 <nozaina.fatima2015@email.com>
2025-12-21 15:25:52 +0500

    Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+new_file=open('newfile.txt','x')
+new_file.close()
+import os
+print("Checking if my file exists or not")
+if os.path.exists('myfile.txt'):
+    os.remove('myfile.txt')
+else:
+    print("The file does not exist")
+
+my_file=open('myfile.txt','w')
+my_file.write("Hi! I am Penguin and I am 11 years old.")
+my_file.close()
+
+os.remove('Codingal.txt')
